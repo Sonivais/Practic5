11abd8dcc7dd2d6020e5bcee6f945494674fb4fe
Sonivais <151126779+Sonivais@users.noreply.github.com>
2023-11-26 23:48:44 +0300

    Add files via upload

---
+++ P5.Zadanie_1/P5.Zadanie_1/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P5.Zadanie_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if ((String.IsNullOrEmpty(TbNumber.Text)))
+            {
+                return;
+            }
+            try
+            {
+                int xa = Convert.ToInt32(TbNumber.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            ListBoxData.Items.Add(TbNumber.Text);
+        }
+
+        private void ButtonOKClick(object sender, RoutedEventArgs e)
+        {
+            int k = 0;
+            try
+            {
+                int n = Convert.ToInt32(ListBoxData.Items[0]);
+                for (int i = 1; i <= n; i++)
+                {
+                    int m = Convert.ToInt32(ListBoxData.Items[i]);
+
+                    if (m % 3 == 0)
+                    {
+                        k++;
+                    }
+                }
+                Answer.Text = $"Ответ:\n{k}";
+            }
+            catch (Exception ex)
+            {
+                Answer.Text += ex.Message;
+            }
+        }
+
+        private void ButtonCancelClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ P5.Zadanie_2/P5.Zadanie_2/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P5.Zadanie_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if ((String.IsNullOrEmpty(TextBox2.Text)))
+            {
+                return;
+            }
+            try
+            {
+                int xa = Convert.ToInt32(TextBox1.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            ListBoxData.Items.Add($"{TextBox1.Text}\n{TextBox2.Text}");
+        }
+
+        private void ButtonOKClick(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(TextBox1.Text, out int a) && int.TryParse(TextBox2.Text, out int b))
+            {
+                int count = 0;
+                for (int i = a; i <= b; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        count++;
+                    }
+                }
+                Answer.Text += $"Всего четных чисел {count}\n";
+            }
+        }
+
+        private void ButtonCancelClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
